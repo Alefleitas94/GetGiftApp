5a315fe671bcacd21b06d6fde99edf05b028a7cd
Alejandro Fleitas <Alefleitas_94@hotmail.com>
2021-09-05 19:20:24 -0300

    Add docs

---
+++ docs/precache-manifest.366df629f5e9661ebc8554df310ae935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76baa2ac101f3925ad007bd4dc89f785",
+    "url": "/index.html"
+  },
+  {
+    "revision": "536fa7d9312cde0a322b",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "536fa7d9312cde0a322b",
+    "url": "/static/js/main.c2ea9fe9.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
