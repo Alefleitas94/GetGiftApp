69718d523579109d745b47b2de3c3c7cece85e38
Alejandro Fleitas <Alefleitas_94@hotmail.com>
2021-09-05 19:53:16 -0300

    Update GetGift componentl

--- src/GetGiftApp.js
+++ src/GetGiftApp.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
-export const GifExpertApp = () => {
+export const GetGiftApp = () => {
     
     const [categories, setCategories] = useState(['Tom y Jerry']);
 
     return (
         <>
-            <h2>Get Gifts</h2>
+            <h2>Get Gifts App</h2>
             <AddCategory setCategories={ setCategories } />
             <hr />
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { GifExpertApp } from './GifExpertApp';
+import { GetGiftApp } from './GetGiftApp';
 
 import './index.css';
 
 ReactDOM.render(
-  <GifExpertApp />,
+  <GetGiftApp />,
   document.getElementById('root')
 );
 
